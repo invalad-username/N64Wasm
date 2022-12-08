376c7bae0147065ff57e5a1aa9c4d109fce2078d
invalad-username <120050360+invalad-username@users.noreply.github.com>
2022-12-08 10:51:10 -0600

    Update romlist.js

--- dist/romlist.js
+++ dist/romlist.js
@@ -1,6 +1,6 @@
 var ROMLIST = [
     /*
-    {url:"roms/rom1.z64",title:"Game 1"},
+    {url:"roms/rom1.z64",title:"Super Mario 64"},
     {url:"roms/rom2.v64",title:"Game 2"},
     {url:"roms/rom3.v64",title:"Game 3"},
     */
